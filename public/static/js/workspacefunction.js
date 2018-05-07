3fb820eb845c434c1b7f1f263c4e63d6987180f8
CNKennyPan <panluzijian@hotmail.com>
2018-05-07 18:34:04 +0800

    2018/5/7

--- public/static/js/workspacefunction.js
+++ public/static/js/workspacefunction.js
@@ -25,10 +25,16 @@ function showpersonalbusiness(tab){
     {},
 	function(data,status){
 		$('#mainworkspace').html(data);
-		myworkrecord = new pagination($('#myworkrecord').html(),'myworkrecord');
+		//分页
+		myworkrecord = new pagination($('#myworkrecord').html(),'myworkrecord',10);
 		$('#myworkrecord').append(myworkrecord.nav);
+		myworkrecord.signupClick('myworkrecord');
+		
+		mywork = new pagination($('#mywork').html(),'mywork',10);
+		$('#mywork').append(mywork.nav);
+		mywork.signupClick('mywork');
 		//pagination($('#mywork').html(),'mywork');
-		//pagination($('#myworkrecord').html(),'myworkrecord');
+		//pagination($('#myworkrecord').html(),'mywork	record');
 		//注册审批按钮点击事件
 		$('.businessshowsubmit').click(function(){
 
@@ -157,6 +163,10 @@ function showpersonnelmanagement(){
     {},
 	function(data,status){
 		$('#mainworkspace').html(data);
+		//分页
+		otpostform = new pagination($('#otpostform').html(),'otpostform',5);
+		$('#otpostform').append(otpostform.nav);
+		otpostform.signupClick('otpostform');
 		//注册查看按钮点击事件
 		$('.businessshowread').click(function(){
 			$.post("/index/business_form/show",
@@ -485,64 +495,184 @@ function formreadonly(formname,number){
 }
 
 //分页
-function pagination(content,sign)
+function pagination(content,sign,userpage)
 {
     this.sign=sign;
 	this.alltr=content.match(/\<tr\>/g);
-	this.prepagenumber=3;
-	this.page=Math.ceil(this.alltr.length/this.prepagenumber);
+	this.prepagenumber=userpage;
+	this.page=Math.ceil((this.alltr.length-1)/this.prepagenumber);
 	this.temp=content;
-	this.navhead='<nav aria-label="Page navigation" id='+this.sign+'nav ><ul class="pagination">';
+	this.navhead='<div class="text-center"><nav aria-label="Page navigation" id='+this.sign+'nav ><ul class="pagination">';
 	this.Previous='<li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
 	this.navbody='';
 	this.next='<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
-	this.navfoot=' </ul></nav>';
+	this.navfoot=' </ul></nav></div>';
 	this.nav='';
 	this.now=1;
-	//Previous\next
+	this.column=0;
+	this.pageselect='<select id="'+this.sign+'navselect"><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="60">60</option></select>';
 	
 	
-	//初始化：
+	//【初始化】
+	
+	//获取列数
+	var columntemp=content.match(/\<tr\>\S+\<\/tr\>/);
+	var columntemp2=columntemp[0].match(/\<th\>/g);
+	this.column=columntemp2.length;
+	
+
+	var addtemp1=this.temp.substr(0,this.temp.lastIndexOf('</tr>'));
+	var addtemp3=this.temp.substr(this.temp.lastIndexOf('</tr>'));
+    var addtemp2='';
 	
 	
-	//添加标记并隐藏
-    for(id in this.alltr){
+	//填充空白位置
+	for(var i=0;i<=(this.alltr.length%this.prepagenumber);i++){
+		addtemp2=addtemp2+'<tr>';
+		for(var b=0;b<this.column;b++){
+			addtemp2=addtemp2+'<td>';
+		}
+		addtemp2=addtemp2+'</tr>';
+	}
+	this.temp = addtemp1+addtemp2+addtemp3;
+	//alert(addtemp2);
+	
+	
+	//添加标记
+    for(id in (this.alltr+this.alltr.length%this.prepagenumber)){
 		this.content=this.temp.replace("<tr>","<tr "+"id="+this.sign+'tb'+id+">");
 		this.temp=this.content;
-		$('#myworkrecordtb0').hide();
-		alert('隐藏');
 	}
 	
 	//重新添加表格
 	$('#'+sign).html(this.content);
 	
+	//alert(this.column);
+	
 	//检测当前页数并变化导航栏
 	if(this.now==1){
-		this.Previous='<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		this.Previous='<li class="disabled"><a href="javascript:void(0)" aria-label="Previous" id="'+this.sign+'navpagerevious"><span aria-hidden="true">&laquo;</span></a></li>';
 	}else{
-		this.Previous='<li><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
+		this.Previous='<li><a href="javascript:void(0)" aria-label="Previous" id="'+this.sign+'navpagerevious"><span aria-hidden="true">&laquo;</span></a></li>';
 	}
 	if(this.now-this.page==0){
-		this.next='<li class="disabled"><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		this.next='<li class="disabled"><a href="javascript:void(0)" aria-label="Next" id="'+this.sign+'navnex"><span aria-hidden="true">&raquo;</span></a></li>';
 	}else{
-		this.next='<li><a href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
+		this.next='<li><a href="javascript:void(0)" aria-label="Next" id="'+this.sign+'navnex"><span aria-hidden="true">&raquo;</span></a></li>';
 	}
 	
 	//合拼导航栏
-	for(var i=0;i<this.page;i++){
-		this.navbody=this.navbody+'<li><a href="#">'+(i+1)+'</a></li>';
+	for(var i=1;i<=this.page;i++){
+		if(i==1){
+			this.navbody=this.navbody+'<li class="active"><a href="javascript:void(0)" class="'+this.sign+'navpage" id="'+this.sign+'navpage'+i+'">'+i+'</a></li>';
+		}else{
+			this.navbody=this.navbody+'<li><a href="javascript:void(0)" class="'+this.sign+'navpage" id="'+this.sign+'navpage'+i+'">'+i+'</a></li>';
+		}
+		
 	}
 	this.nav=this.navhead+this.Previous+this.navbody+this.next+this.navfoot;
 	
 	
-	for(var i=0;i<this.prepagenumber;i++){
-		//$('#'+this.sign+'tb'+i).show();
+	//注册页数点击事件
+	this.signupClick=signupClick;
+	function signupClick(sign){
+		//$('.'+this.sign+'navpage').on("click", {page:"123"}, this.selectOperate);
+		$('#'+this.sign+'navpagerevious').click({sign:sign},this.previousOperate)
+		$('#'+this.sign+'navnex').click({sign:sign},this.nextOperate);
+		$('.'+this.sign+'navpage').click({sign:sign},this.selectOperate);
 	}
 	
 	
 	
+	//显示表格
+	this.showtb=showtb;
+	function showtb(){
+		//隐藏所有
+		for(id in (this.alltr+this.alltr.length%this.prepagenumber)){
+			//alert('隐藏'+'#'+this.sign+'tb'+id);
+			$('#'+this.sign+'tb'+id).hide();
+		}
+		//显示表头
+		$('#'+this.sign+'tb'+0).show();
+		//显示内容
+		for(var i=(this.now-1)*this.prepagenumber+1;i<=this.now*this.prepagenumber;i++){
+		//alert('显示'+'#'+this.sign+'tb'+i);
+		//alert(this.now);
+		$('#'+this.sign+'tb'+i).show();
+		}
+		
+	}
+	
+	this.showtb();
+	
+	//前一页操作
+	this.previousOperate=previousOperate;
+    function previousOperate(event)
+    {
+		var tempnow =eval(event.data.sign+'.now');
+		if((tempnow-1)==0){
+			alert('现在已经是第一页');
+		}else{
+		eval(event.data.sign+'.now='+event.data.sign+'.now-1');
+		eval(event.data.sign+'.showtb()');
+		eval(event.data.sign+'.navRenovate()');
+		}
+		eval(event.data.sign+'.navActive()');
+		//alert(eval(event.data.sign+'.now'));
+    }
 	
 	
+	//后一页操作
+	this.nextOperate=nextOperate;
+    function nextOperate(event)
+    {
+		var tempnow =eval(event.data.sign+'.now');
+		var temppage =eval(event.data.sign+'.page');
+		if(tempnow+1>temppage){
+			alert('现在已经是最后一页');
+		}else{
+			eval(event.data.sign+'.now='+event.data.sign+'.now+1');
+			eval(event.data.sign+'.showtb()');
+			eval(event.data.sign+'.navRenovate()');	
+		}
+		eval(event.data.sign+'.navActive()');
+        //alert(eval(event.data.sign+'.now'));
+    }
+	
+	//选取页操作
+	this.selectOperate=selectOperate;
+    function selectOperate(event)
+    {
+		var temppage = event.currentTarget.id;
+        eval(event.data.sign+'.now=temppage.match(/[0-9]+/)');
+		eval(event.data.sign+'.showtb()');
+		eval(event.data.sign+'.navRenovate()');
+		eval(event.data.sign+'.navActive()');
+		//alert(eval(event.data.sign+'.now'));
+	}
+	
+	//导航栏刷新
+	this.navRenovate=navRenovate;
+	function navRenovate(){
+		if(this.now==1){
+			$('#'+this.sign+'navpagerevious').parent().addClass("disabled");
+			$('#'+this.sign+'navnex').parent().removeClass("disabled");
+		}else if(this.now-this.page==0){
+			$('#'+this.sign+'navpagerevious').parent().removeClass("disabled");
+			$('#'+this.sign+'navnex').parent().addClass("disabled");
+		}
+		//alert('导航栏已刷新');
+		//alert('#'+this.sign+'navpagerevious')
+	}
+
+	//激活刷新
+	this.navActive=navActive;
+	function navActive(){
+		for(var i=0;i<=this.page;i++){
+			$('#'+this.sign+'navpage'+i).parent().removeClass('active');
+		}
+		$('#'+this.sign+'navpage'+this.now).parent().addClass('active');
+	}
 	
 }
 
